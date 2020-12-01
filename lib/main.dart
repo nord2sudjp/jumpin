d8b1b0bc4390babaa78ba7da18f7d64f5cf3729a
maka y <nord2sudjp@gmail.com>
2020-12-01 18:07:15 +0900

    bottom navigator

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,8 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int _selectPageIndex = 0;
+
     final MX = MediaQuery.of(context);
     final isLandscape = MX.orientation == Orientation.landscape;
     final PreferredSizeWidget appBar = Platform.isIOS
@@ -99,40 +101,55 @@ class MyHomePage extends StatelessWidget {
                 )
               ]);
     return Scaffold(
-        appBar: appBar,
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: (MX.size.height -
+                      appBar.preferredSize.height -
+                      MX.padding.top) *
+                  0.1,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("マンスリー課題",
+                        style: TextStyle(fontSize: 15, color: Colors.red[400])),
+                    Text("テープ課題",
+                        style: TextStyle(fontSize: 15, color: Colors.red[400])),
+                    Text("特別課題",
+                        style: TextStyle(fontSize: 15, color: Colors.red[400]))
+                  ]),
+            ),
+            Container(
                 height: (MX.size.height -
                         appBar.preferredSize.height -
                         MX.padding.top) *
-                    0.1,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text("マンスリー課題",
-                          style:
-                              TextStyle(fontSize: 15, color: Colors.red[400])),
-                      Text("テープ課題",
-                          style:
-                              TextStyle(fontSize: 15, color: Colors.red[400])),
-                      Text("特別課題",
-                          style:
-                              TextStyle(fontSize: 15, color: Colors.red[400]))
-                    ]),
-              ),
-              Container(
-                  height: (MX.size.height -
-                          appBar.preferredSize.height -
-                          MX.padding.top) *
-                      0.9,
-                  child: MonthlyHeader(monthlymaster: monthlymaster)),
-            ],
+                    0.9,
+                child: MonthlyHeader(monthlymaster: monthlymaster)),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: null,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
           ),
-        ));
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
   }
 }
