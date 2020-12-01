da7e950d024f706486fd157872a027d212d85378
maka y <nord2sudjp@gmail.com>
2020-12-01 17:20:59 +0900

    show in singledhildscrollview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import './models/monthly_master.dart';
 import 'widgets/montly_header.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<MonthlyMaster> monthlymaser = [
+  final List<MonthlyMaster> monthlymaster = [
     MonthlyMaster(year: 2020, month: 8),
     MonthlyMaster(year: 2020, month: 7),
     MonthlyMaster(year: 2020, month: 6),
@@ -37,20 +39,23 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MX = MediaQuery.of(context);
+    final isLandscape = MX.orientation == Orientation.landscape;
+    final PreferredSizeWidget appBar = Platform.isIOS
+        ? CupertinoNavigationBar()
+        : AppBar(title: Text("課題1"), actions: <Widget>[]);
     return Scaffold(
-        appBar: AppBar(
-          title: Text("課題"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Column(
-              children: monthlymaser.map((mm) {
-                return MonthlyHeader(year: mm.year, month: mm.month);
-              }).toList(),
-            ),
-          ],
+        appBar: appBar,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                  height: 400,
+                  child: MonthlyHeader(monthlymaster: monthlymaster)),
+            ],
+          ),
         ));
   }
 }
--- lib/widgets/montly_header.dart
+++ lib/widgets/montly_header.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import '../models/monthly_master.dart';
 
 class MonthlyHeader extends StatelessWidget {
-  final int year;
-  final int month;
-  MonthlyHeader({this.year, this.month});
+  //final int year;
+  //final int month;
+  final List<MonthlyMaster> monthlymaster;
+
+  MonthlyHeader({this.monthlymaster});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 5),
-      elevation: 1,
-      child: ListTile(
-        leading: Icon(Icons.folder_open_outlined),
-        title: Text(year.toString() + "年" + month.toString() + "月"),
-        trailing: Icon(Icons.arrow_forward_ios),
-      ),
-    );
+    print(monthlymaster.toString());
+    return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            margin: EdgeInsets.symmetric(vertical: 4, horizontal: 5),
+            elevation: 1,
+            child: ListTile(
+              leading: Icon(Icons.folder_open_outlined),
+              title: Text(monthlymaster[index].year.toString() +
+                  "年" +
+                  monthlymaster[index].month.toString() +
+                  "月"),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          );
+        },
+        itemCount: monthlymaster.length);
   }
 }
