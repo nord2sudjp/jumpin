0d2fbfd4cf3514683ed5ec1d297f04468831b3b1
maka y <nord2sudjp@gmail.com>
2020-12-01 17:46:37 +0900

    import material color

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const MaterialColor mcgpalette0 =
+      MaterialColor(_mcgpalette0PrimaryValue, <int, Color>{
+    50: Color(0xFFFBEDED),
+    100: Color(0xFFF6D1D1),
+    200: Color(0xFFF0B3B3),
+    300: Color(0xFFE99494),
+    400: Color(0xFFE57D7D),
+    500: Color(_mcgpalette0PrimaryValue),
+    600: Color(0xFFDC5E5E),
+    700: Color(0xFFD85353),
+    800: Color(0xFFD34949),
+    900: Color(0xFFCB3838),
+  });
+  static const int _mcgpalette0PrimaryValue = 0xFFE06666;
+
+  static const MaterialColor mcgpalette0Accent =
+      MaterialColor(_mcgpalette0AccentValue, <int, Color>{
+    100: Color(0xFFFFFFFF),
+    200: Color(_mcgpalette0AccentValue),
+    400: Color(0xFFFFAFAF),
+    700: Color(0xFFFF9696),
+  });
+  static const int _mcgpalette0AccentValue = 0xFFFFE2E2;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Jump In!',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.amber,
+        primarySwatch: mcgpalette0,
+        accentColor: mcgpalette0Accent,
         fontFamily: 'QuickSand',
         textTheme: ThemeData.light().textTheme.copyWith(
               headline6: TextStyle(
