03a7abcbb98843f8228d6418f3410badf02d9fed
maka y <nord2sudjp@gmail.com>
2020-12-04 21:47:28 +0900

    refurbish homepage screen

--- lib/screen/homepage_screen.dart
+++ lib/screen/homepage_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'dart:io';
-
-import '../widgets/bottom_navi.dart';
 
 class HomepageScreen extends StatelessWidget {
   static const routeName = './homepage';
@@ -10,31 +7,17 @@ class HomepageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final MX = MediaQuery.of(context);
-    final isLandscape = MX.orientation == Orientation.landscape;
-    final PreferredSizeWidget appBar = Platform.isIOS
-        ? CupertinoNavigationBar()
-        : AppBar(
-            title: Text(
-              "ホームページ",
-              textAlign: TextAlign.center,
-            ),
-            actions: <Widget>[Text("ブラウザで開く")]);
-    return Scaffold(
-      appBar: appBar,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: (MX.size.height -
-                      appBar.preferredSize.height -
-                      MX.padding.top) *
-                  1.0,
-              child: Text("ホームページ"),
-            )
-          ],
-        ),
+
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: (MX.size.height - MX.padding.top) * 1.0,
+            child: Text("ホームページ"),
+          )
+        ],
       ),
     );
   }
