8b60f06e91f0a01f22d928ed81d25b22621ca581
maka y <nord2sudjp@gmail.com>
2020-12-05 22:32:41 +0900

    sample tab screen

--- lib/screen/SampleTabScreen.dart
+++ lib/screen/SampleTabScreen.dart
@@ -5,7 +5,22 @@ class SampleTabScreen extends StatefulWidget {
   _SampleTabScreenState createState() => _SampleTabScreenState();
 }
 
-class _SampleTabScreenState extends State<SampleTabScreen> {
+class _SampleTabScreenState extends State<SampleTabScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: _tab.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   final _tab = <Tab>[
     Tab(text: 'Car', icon: Icon(Icons.directions_car)),
     Tab(text: 'Bicycle', icon: Icon(Icons.directions_bike)),
@@ -13,25 +28,27 @@ class _SampleTabScreenState extends State<SampleTabScreen> {
   ];
 
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: _tab.length,
-      child: Scaffold(
-        appBar: AppBar(
-          flexibleSpace: Column(
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: SafeArea(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              TabBar(
-                tabs: _tab,
+            children: [
+              Expanded(
+                child: TabBar(
+                  controller: _tabController,
+                  tabs: _tab,
+                ),
               ),
             ],
           ),
         ),
-        body: TabBarView(children: <Widget>[
-          TabPage(title: 'Car', icon: Icons.directions_car),
-          TabPage(title: 'Bicycle', icon: Icons.directions_bike),
-          TabPage(title: 'Boat', icon: Icons.directions_boat),
-        ]),
       ),
+      body: TabBarView(controller: _tabController, children: <Widget>[
+        TabPage(title: 'Car', icon: Icons.directions_car),
+        TabPage(title: 'Bicycle', icon: Icons.directions_bike),
+        TabPage(title: 'Boat', icon: Icons.directions_boat),
+      ]),
     );
   }
 }
@@ -47,7 +64,7 @@ class TabPage extends StatelessWidget {
     final TextStyle textStyle = Theme.of(context).textTheme.display1;
     return Center(
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        //mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Icon(icon, size: 64.0, color: textStyle.color),
